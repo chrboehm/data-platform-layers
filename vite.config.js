566bc52b069fb249e0b8c0da23495e5a5141b0bf
Christoph Böhm <christoph.boehm@bakdata.com>
2025-06-18 13:46:06 +0200

    gh pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/treewip/',
+  base: '/data-platform-layers/',
   plugins: [react()],
 })
